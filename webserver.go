3c94f96ed54d9a80bb8b9438be9405fb7cbc1c09
Leonardo Souza <leogsouza@gmail.com>
2018-11-25 22:42:57 -0200

    Add singleton logger

--- webserver.go
+++ webserver.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/leogsouza/hydra/hlogger"
 )
 
 func main() {
+	logger := hlogger.GetInstance()
+	logger.Println("Starting Hydra web service")
 	http.HandleFunc("/", sroot)
 	http.ListenAndServe(":8080", nil)
 }
 
 func sroot(w http.ResponseWriter, r *http.Request) {
+	logger := hlogger.GetInstance()
+
 	fmt.Fprintln(w, "Welcome to Hydra software system.")
+
+	logger.Println("Received an http Get request on root url")
 }
